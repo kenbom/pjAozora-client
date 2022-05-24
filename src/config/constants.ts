3c345f70387b37a133b07b06e89209d63c4fbd3c
kenbom <tsutomu.kimura@nifty.ne.jp>
2022-05-24 22:36:15 +0000

    modify for deploy

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:4000/graphql';
+// export const BASE_URL = 'http://localhost:4000/graphql';
+export const BASE_URL = 'https://git.heroku.com/pjaozora-server.git:4000/graphql';
